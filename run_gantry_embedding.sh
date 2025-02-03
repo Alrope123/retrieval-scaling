9d4fa76b900bdf48eb4cd79bcfaacbff6405c58a
Sewon Min <sewon@cs.washington.edu>
2025-02-03 22:40:04 +0000

    change replicas

--- run_gantry_embedding.sh
+++ run_gantry_embedding.sh
@@ -17,7 +17,7 @@ gantry run \
     --priority "${PRIORITY}" \
     --leader-selection \
     --gpus 1 \
-    --replicas 128 \
+    --replicas 64 \
     --preemptible \
     --cluster "${CLUSTER}" \
     --budget ai2/oe-data \
