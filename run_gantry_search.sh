7fec4dbb2f4feac46b08c8b8f8bc799033455836
Sewon Min <sewon@cs.washington.edu>
2025-02-06 01:00:11 +0000

    add all

--- run_gantry_search.sh
+++ run_gantry_search.sh
@@ -1,16 +1,15 @@
 
-CLUSTER="ai2/jupiter*"
+CLUSTER="ai2/jupiter*,ai2/saturn*,ai2/neptune*"
 PRIORITY="high"
 
 export BEAKER_EXPERIMENT_NAME="Contriever-search"
-
-pip install -e rag-evaluation-harness
 
 config_name=dclm_ft7percentile_fw3_dense_retrieval 
 
 RETRIEVED_FILE=/data/input/sewonm/dense-retrieval/dclm_ft7percentile_fw3_shard00/retrieved_results/facebook/contriever-msmarco/0_datastore-256_chunk_size/top_3/mmlu_retrieved_results.jsonl  # where retrieved documents are saved
 
 command='
+pip install -e rag-evaluation-harness;
 lm_eval --tasks "mmlu" --inputs_save_dir "examples" --save_inputs_only ;
 PYTHONPATH=.  python ric/main_ric.py --config-name $config_name \
 	tasks.eval.task_name=lm-eval \
@@ -32,7 +31,7 @@ gantry run \
     --task-name "Contriever-search" \
     --description "Search for dense retrieval" \
     --allow-dirty \
-    --workspace ai2/oe-data \
+    --workspace ai2/ds-olmo \
     --beaker-image 'petew/olmo-torch23-gantry' \
     --timeout -1 \
     --show-logs \
@@ -41,7 +40,7 @@ gantry run \
     --priority "${PRIORITY}" \
     --leader-selection \
     --gpus 1 \
-    --replicas 24 \
+    --replicas 1 \
     --preemptible \
     --cluster "${CLUSTER}" \
     --budget ai2/oe-data \
