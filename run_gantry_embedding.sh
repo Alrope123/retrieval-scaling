c2eac8172ade7623a7acafb2adf80444218109d6
Sewon Min <sewon@cs.washington.edu>
2025-02-09 08:59:08 +0000

    add dclm full

--- run_gantry_embedding.sh
+++ run_gantry_embedding.sh
@@ -31,7 +31,7 @@ gantry run \
     --shared-memory 10GiB \
     --weka oe-data-default:/data \
     --yes \
-    -- python -m ric.main_ric --config-name c4_dense_retrieval tasks.datastore.embedding=true
+    -- python -m ric.main_ric --config-name dclm_ft7percentile_fw3_full_dense_retrieval tasks.datastore.embedding=true
 
 
 #    --beaker-image 'petew/olmo-torch23-gantry' \
