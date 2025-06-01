29f8c3b7adb73bf722b70026541a3521c60ee578
root <root@neptune-cs-aus-262.reviz.ai2.in>
2025-05-31 19:15:05 -0700

    fixed config

--- run_gantry_embedding.sh
+++ run_gantry_embedding.sh
@@ -1,6 +1,6 @@
 
-CLUSTER="ai2/jupiter*"
-PRIORITY="high"
+CLUSTER="ai2/jupiter-cirrascale-2"
+PRIORITY="urgent"
 
 export BEAKER_EXPERIMENT_NAME="Contriever-embedding"
 
@@ -8,7 +8,7 @@ gantry run \
     --task-name "Contriever-embedding-$1" \
     --description "Embed docs for dense retrieval $1" \
     --allow-dirty \
-    --workspace ai2/ds-olmo \
+    --workspace ai2/OLMo-modular \
     --beaker-image 'lucas/refine1' \
     --timeout -1 \
     --show-logs \
@@ -17,7 +17,7 @@ gantry run \
     --priority "${PRIORITY}" \
     --leader-selection \
     --gpus 1 \
-    --replicas 8 \
+    --replicas 1 \
     --cluster "${CLUSTER}" \
     --budget ai2/oe-data \
     --env LOG_FILTER_TYPE=local_rank0_only \
@@ -26,9 +26,11 @@ gantry run \
     --env-secret AWS_ACCESS_KEY_ID=SEWONM_AWS_ACCESS_KEY_ID \
     --env-secret AWS_SECRET_ACCESS_KEY=SEWONM_AWS_SECRET_ACCESS_KEY \
     --env-secret WANDB_API_KEY=SEWONM_WANDB_API_KEY \
-    --install "pip install necessary platformdirs>=4.2.0 smart-open fsspec>=2023.6.0" \
+    --preemptible \
+    --install "pip install faiss-cpu==1.8.0 omegaconf hydra-core tqdm transformers sentence_transformers pyserini datasketch boto3 smart_open s3fs necessary platformdirs>=4.2.0 smart-open fsspec>=2023.6.0 && pip install --force-reinstall -U --no-deps -v gritlm" \
     --shared-memory 10GiB \
     --weka oe-data-default:/weka_data \
+    --weka oe-training-default:/weka_training \
     --yes \
     -- python -m ric.main_ric --config-name $1 tasks.datastore.embedding=true
 
