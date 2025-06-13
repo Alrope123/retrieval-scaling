cb430fade14e29d63223132d8ed0aadd05af5d9f
root <root@neptune-cs-aus-262.reviz.ai2.in>
2025-06-13 00:24:59 -0700

    added bash file for downloading compactds

---
+++ scripts/download_compactds.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Check if an argument is passed
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 <argument>"
+  exit 1
+fi
+
+output_dir=$1
+
+# Download the sharded index files
+python scripts/download_index.py --output_path output_dir
+
+# Combine the shards
+cat output_dir/embeddings/index_IVFPQ/index_IVFPQ.100000000.768.65536.64.faiss.meta* > index_IVFPQ.100000000.768.65536.64.faiss.meta
+cat output_dir/embeddings/index_IVFPQ/index_IVFPQ.100000000.768.65536.64.faiss* > index_IVFPQ.100000000.768.65536.64.faiss
+
+# Remove shard files
+rm output_dir/embeddings/index_IVFPQ/index_IVFPQ.100000000.768.65536.64.faiss.meta_*
+rm output_dir/embeddings/index_IVFPQ/index_IVFPQ.100000000.768.65536.64.faiss_*
